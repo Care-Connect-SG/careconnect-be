3c17e9c7377422d99b9015dcadc2c6ce62048422
eunice0361 <eunice_lor@htx.gov.sg>
2025-02-25 23:52:38 +0800

    Refractor: Updated User Model

--- models/user.py
+++ models/user.py
@@ -1,11 +1,37 @@
-from pydantic import BaseModel
+from enum import Enum
+from pydantic import BaseModel, Field, EmailStr, field_validator
+from typing import Optional
+from db.base import PyObjectId, BaseConfig
+from auth.hashing import Hash
+
+class Role(Enum):
+    ADMIN = "ADMIN"
+    NURSE = "NURSE"
+    FAMILY = "FAMILY" 
+
+class Gender(Enum):
+    MALE = "MALE"
+    FEMALE = "FEMALE"
 
 class User(BaseModel):
-    email: str
+    id: PyObjectId = Field(..., alias="_id")  # This is now compulsory, so '...' means it must be provided
+    email: EmailStr
+    name: str
     password: str
+    contact_number: Optional[str] = None
+    role: Role
+    organisation_rank: Optional[str] = None
+    gender: Gender
+
+    # Hash password before saving
+    @field_validator('password')
+    def hash_password(cls, value):
+        return Hash.bcrypt(value)
+    
+    class Config(BaseConfig):
+        pass
 
 class Token(BaseModel):
     access_token: str
     token_type: str
     email: str
-
