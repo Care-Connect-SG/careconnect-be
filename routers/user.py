ede2bc60c23ce14f2654a0fa301bb0342b7c2627
notsushiii <sushweyeemdy@gmail.com>
2025-03-04 23:30:44 +0800

    chore: remove unused import

--- routers/user.py
+++ routers/user.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, APIRouter, HTTPException, status, Request
+from fastapi import Depends, APIRouter, status, Request
 from fastapi.security import OAuth2PasswordRequestForm
 from db.connection import get_db
 from models.user import UserCreate, UserResponse, Token
