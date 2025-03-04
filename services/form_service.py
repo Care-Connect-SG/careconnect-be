40a384e84ba8a0d8906aef906cf422627eb35e3e
notsushiii <sushweyeemdy@gmail.com>
2025-03-04 11:33:27 +0800

    fix: disallow response model by alias

--- services/form_service.py
+++ services/form_service.py
@@ -13,7 +13,6 @@ from bson import ObjectId
 async def create_form(form: FormCreate, db) -> str:
     form_data = form.model_dump()
     form_data["created_date"] = str(datetime.now())
-    form_data["_id"] = ObjectId()
     result = await db["forms"].insert_one(form_data)
     print("inserted_id: ", result.inserted_id)
     return str(result.inserted_id)
