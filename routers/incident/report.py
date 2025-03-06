aa290e8c7543d1b536f38822519bf0a4c4ed0958
notsushiii <sushweyeemdy@gmail.com>
2025-03-06 22:26:57 +0800

    fix: fix report endpoints

--- routers/incident/report.py
+++ routers/incident/report.py
@@ -1,29 +1,58 @@
-from typing import Optional
-from fastapi import APIRouter, Depends
+from typing import List, Optional
+from fastapi import APIRouter, Depends, Request
 
 from db.connection import get_db
-from models.report import ReportCreate
+from models.report import ReportCreate, ReportResponse
 from services.report_service import create_report, get_report_by_id, get_reports, remove_report, update_report
+from utils.limiter import limiter
 
 
 router = APIRouter(prefix="/incident/reports", tags=["Incident Management Subsystem"])
 
-@router.post("/")
-async def create_new_report(report: ReportCreate, db=Depends(get_db)):
+
+@router.post("/", summary="Create a new incident report", response_model=str)
+@limiter.limit("10/minute")
+async def create_new_report(request: Request, report: ReportCreate, db=Depends(get_db)):
     return await create_report(report, db)
 
-@router.get("/")
-async def list_reports(status: Optional[str] = None, db=Depends(get_db)):
+
+@router.get(
+    "/",
+    summary="Retrieve incident reports",
+    response_model=List[ReportResponse],
+    response_model_by_alias=False    
+)
+@limiter.limit("100/minute")
+async def list_reports(request: Request, status: Optional[str] = None, db=Depends(get_db)):
     return await get_reports(status, db)
 
-@router.get("/{report_id}")
-async def get_report(report_id: str, db=Depends(get_db)):
+
+@router.get(
+    "/{report_id}",
+    summary="Retrieve a specific report",
+    response_model=ReportResponse,
+    response_model_by_alias=False
+)
+@limiter.limit("100/minute")
+async def get_report(request: Request, report_id: str, db=Depends(get_db)):
     return await get_report_by_id(report_id, db)
 
-@router.put("/{report_id}")
-async def edit_report(report_id: str, report: ReportCreate, db=Depends(get_db)):
+
+@router.put(
+    "/{report_id}",
+    summary="Update an existing report",
+    response_model=str
+)
+@limiter.limit("10/minute")
+async def edit_report(request: Request, report_id: str, report: ReportCreate, db=Depends(get_db)):
     return await update_report(report_id, report, db)
 
-@router.delete("/{report_id}")
-async def delete_report(report_id: str, db=Depends(get_db)):
+
+@router.delete(
+    "/{report_id}",
+    summary="Delete a report",
+    response_model=str
+)
+@limiter.limit("10/minute")
+async def delete_report(request: Request, report_id: str, db=Depends(get_db)):
     return await remove_report(report_id, db)
--- services/report_service.py
+++ services/report_service.py
@@ -1,48 +1,50 @@
 from datetime import datetime
+from typing import List
 from bson import ObjectId
 from fastapi import HTTPException
-from models.report import ReportCreate
+from models.report import ReportCreate, ReportResponse
 
 
-async def create_report(report: ReportCreate, db):
+async def create_report(report: ReportCreate, db) -> str:
     report_data = report.model_dump()
-    report_data["created_date"] = datetime.now()
+    report_data["created_date"] = str(datetime.now())
     result = await db["reports"].insert_one(report_data)
     return str(result.inserted_id)
 
 
-async def get_reports(status: str, db):
+async def get_reports(status: str, db) -> List[ReportResponse]:
     query = {}
     if status:
         query["status"] = status
     cursor = db["reports"].find(query)
     reports = []
     async for report in cursor:
-        report["id"] = str(report["_id"])
         reports.append(report)
-    return reports
+    return [ReportResponse(**report) for report in reports]
 
 
-async def get_report_by_id(report_id: str, db):
+async def get_report_by_id(report_id: str, db) -> ReportResponse:
     try:
         object_id = ObjectId(report_id) 
     except Exception:
         raise HTTPException(status_code=400, detail="Invalid report ID format")
 
-    report = await db["reports"].find_one({"_id": ObjectId(report_id)})
+    report = await db["reports"].find_one({"_id": object_id})
     if not report:
         raise HTTPException(status_code=404, detail="Report not found")
     report["_id"] = str(report["_id"])
     return report
 
 
-async def update_report(report_id: str, report: ReportCreate, db):
+async def update_report(report_id: str, report: ReportCreate, db) -> str:
     try:
+        print("report Id: ", report_id)
         object_id = ObjectId(report_id)
     except Exception:
         raise HTTPException(status_code=400, detail="Invalid report ID")
     
-    report_data = await db["report"].find_one({"_id": object_id})
+    report_data = await db["reports"].find_one({"_id": object_id})
+    print("report data: ", report_data)
     if not report_data:
         raise HTTPException(status_code=404, detail="Report not found")
 
