cfe14d53ceac79907e33631f464f6e7dc9bfa79b
notsushiii <sushweyeemdy@gmail.com>
2025-03-06 22:26:46 +0800

    refactor: update report models

--- models/report.py
+++ models/report.py
@@ -1,21 +1,34 @@
-from pydantic import BaseModel
+from enum import Enum
+from pydantic import BaseModel, Field
 from typing import List, Optional
+from models.base import PyObjectId
+from models.resident import ResidentTagResponse
+from models.user import CaregiverTagResponse
+
+
+class ReportStatus(str, Enum):
+    DRAFT = "Draft"
+    PUBLISHED = "Published"
+
 
 class ReportSectionContent(BaseModel):
     form_element_id: str
     input: str | List[str]
 
+
 class ReportCreate(BaseModel):
     form_id: str
-    reporter_id: str
-    primary_resident: Optional[str] = None
-    involved_residents: Optional[List[str]] = []
-    involved_caregivers: Optional[List[str]] = []
+    form_name: str
+    reporter: CaregiverTagResponse
+    primary_resident: Optional[ResidentTagResponse] = None
+    involved_residents: Optional[List[ResidentTagResponse]] = []
+    involved_caregivers: Optional[List[CaregiverTagResponse]] = []
     report_content: List[ReportSectionContent]
     status: str
 
+
 class ReportResponse(ReportCreate):
-    _id: str
+    id: Optional[PyObjectId] = Field(alias="_id", default=None)
     created_date: str
     published_date: Optional[str] = None
     
