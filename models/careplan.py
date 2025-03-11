f806cc7a0a95f01daba373abce4b78a314ffc26f
ng-jayson <jng277270@gmail.com>
2025-03-11 14:29:28 +0800

    chore: remove comments

--- models/careplan.py
+++ models/careplan.py
@@ -8,44 +8,30 @@ class CarePlanCreate(BaseModel):
     created_date: date
     medical_conditions: Optional[str] = None
     doctor_appointments: Optional[str] = None
-
-    # Dietary Plan
     dietary_restrictions: Optional[str] = None
-    daily_meal_plan: Optional[str] = (
-        None  # Store as structured text (e.g., Breakfast: ..., Lunch: ...)
-    )
+    daily_meal_plan: Optional[str] = None
     hydration: Optional[str] = None
     nutritional_supplements: Optional[str] = None
-
-    # Daily Care Routine
     bathing_assistance: Optional[bool] = None
     dressing_assistance: Optional[bool] = None
-    required_assistance: Optional[str] = None  # E.g., walker, wheelchair, cane
-
-    # Social & Recreational Activities
+    required_assistance: Optional[str] = None
     hobbies_interests: Optional[str] = None
-    social_interaction_plan: Optional[str] = None  # E.g., Visits from family/friends
+    social_interaction_plan: Optional[str] = None
 
 
 class CarePlanResponse(BaseModel):
-    id: str  # auto generated
+    id: str
     resident_id: str
     created_date: date
     last_updated: Optional[date] = None
     medical_conditions: Optional[str] = None
     doctor_appointments: Optional[str] = None
-
-    # Dietary Plan
     dietary_restrictions: Optional[str] = None
     daily_meal_plan: Optional[str] = None
     hydration: Optional[str] = None
     nutritional_supplements: Optional[str] = None
-
-    # Daily Care Routine
     bathing_assistance: Optional[bool] = None
     dressing_assistance: Optional[bool] = None
     required_assistance: Optional[str] = None
-
-    # Social & Recreational Activities
     hobbies_interests: Optional[str] = None
     social_interaction_plan: Optional[str] = None
